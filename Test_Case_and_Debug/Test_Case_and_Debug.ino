810d1f7fd1e9a8fc6532342c7c9cdf0570db6d74
IonutHaita <140441324+IonutHaita@users.noreply.github.com>
2023-08-15 03:41:12 +0300

    Create Test_Case_and_Debug.ino
    Not fully tested.

---
+++ Test_Case_and_Debug/Test_Case_and_Debug.ino
@@ -0,0 +1,146 @@
+const int ldrul = 5;//replace value with the coresponding pin address for the upper left LDR
+const int ldrur = 6;//replace value with the coresponding pin address for the upper right LDR
+const int ldrll = 7;//replace value with the coresponding pin address for the lower left LDR
+const int ldrlr = 8;//replace value with the coresponding pin address for the lower right LDR
+const int panel = 9;//replace value with the coresponding pin address for the solar panel
+
+int ulval = analogRead(ldrul);//read value from the upper left sensor and pass it into ulval variable
+int urval = analogRead(ldrur);//read value from the upper right sensor and pass it into urval variable
+int llval = analogRead(ldrll);//read value from the lower left sensor and pass it into llval variable
+int lrval = analogRead(ldrlr);//read value from the upper left sensor and pass it into ulval variable
+int pval =  analogRead(panel);//read value from the upper left sensor and pass it into ulval variable
+
+void displaySensValSingle()
+{
+  Serial.print("Upper left value: ");
+  Serial.println(ulval);
+  Serial.print("Upper right value: ");
+  Serial.println(urval);
+  Serial.print("Lower left value: ");
+  Serial.println(llval);
+  Serial.print("Lower right value: ");
+  Serial.println(lrval);
+  Serial.println("Exiting to Main Menu...");
+  delay(200);
+  preMenu();
+}
+
+void displaySensValCont()
+{
+  while(!Serial.available())
+  {
+    Serial.print("Upper left value: ");
+    Serial.println(ulval);
+    Serial.print("Upper right value: ");
+    Serial.println(urval);
+    Serial.print("Lower left value: ");
+    Serial.println(llval);
+    Serial.print("Lower right value: ");
+    Serial.println(lrval);
+    delay(300);
+  }
+  Serial.println("Exiting to Main Menu...");
+  delay(200);
+  preMenu();
+}
+
+void debugMovement()
+{
+  while(!Serial.available())
+  {
+    const int time = 100; //experiment with time delay and see how often the position is adjusted
+    const int tol = 800; 
+
+    String horizdir = "neutral";
+    String vertidir = "neutral";
+
+    int avgup = (ulval + urval) / 2; 
+    int avgdown = (llval + lrval) / 2;
+    int avgleft = (ulval + llval) / 2;
+    int avgright = (urval + lrval) / 2;
+
+    int dvert = avgup - avgdown;
+    int doriz = avgleft - avgright;
+
+    if (-1*dvert > tol || dvert > tol)
+      {
+        if (avgup > avgdown)
+          {
+            vertidir = "The panel is moving up";
+          }
+        else if (avgup < avgdown)
+          {
+            vertidir = "The panel is moving down";
+          }
+          Serial.println(vertidir);
+      }
+
+    if (-1*doriz > tol || doriz > tol)  
+      {
+        if (avgleft > avgright) 
+          {
+            horizdir = "The panel is moving left"; 
+          }
+        else if (avgleft < avgright)
+          {
+            horizdir = "The panel is moving right";
+          }
+        Serial.println(horizdir);
+      }
+  
+
+    delay(time);
+  }
+  Serial.println("Exiting to Main Menu...");
+  delay(200);
+  preMenu();
+}
+
+void preMenu()
+{
+  Serial.println("========== Main Menu ==========");
+  Serial.println("Select an option:");
+  Serial.println("1. Sensor values - single");
+  Serial.println("2. Sensor values - continuos");
+  Serial.println("3. Debug panel movement");
+  Serial.println("==============================");
+  mainMenu();
+}
+
+void mainMenu()
+{
+  while(1)
+  {
+    if(Serial.available())
+    {
+      int option = Serial.parseInt();
+      Serial.read();
+      switch(option)
+      {
+        case 1:
+        displaySensValSingle();
+        break;
+        case 2:
+        displaySensValCont();
+        break;
+        case 3:
+        debugMovement();
+        break;
+        default:
+        Serial.println("Invalid option, try again!");
+        preMenu();
+        break;
+      }
+    }
+  }
+}
+
+void setup()
+{
+  Serial.begin(9600);//start serial communication
+}
+
+void loop()
+{
+  preMenu();
+}
